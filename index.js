3d59194b612ee5ec9ba368f999488db37cfb3af8
kdub5184 <kdub5184@gmail.com>
2022-04-01 12:49:13 -0400

    shrink big enemies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas')
 canvas.oncontextmenu = function(e) { e.preventDefault(); e.stopPropagation(); }
 const c = canvas.getContext('2d')
+console.log(gsap)
 
 canvas.width = innerWidth;
 canvas.height = innerHeight
@@ -72,7 +73,7 @@ class Enemy {
 const canvasx = canvas.width / 2
 const canvasy = canvas.height / 2
 
-const player = new Player(canvasx, canvasy, 30, 'blue');
+const player = new Player(canvasx, canvasy, 15, 'white');
 
 
 const projectiles = []
@@ -82,7 +83,7 @@ let score = 0
 function spawnEnenies() {
     setInterval (() => {
 
-        this.radius = Math.random() * 24 + 6
+        this.radius = Math.random() * 30 + 7
         let x, y
         if(Math.random() > 0.5) {
             x = Math.random() < 0.5 ? 0-this.radius : canvas.width + this.radius
@@ -91,7 +92,8 @@ function spawnEnenies() {
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0-this.radius : canvas.height + this.radius
         }
-        const color = 'green'
+        const color = 'hsl(' + Math.random() * 360 + ', 100%, 50%, 1)'
+
         const angle = Math.atan2(canvasy - y, canvasx - x)
 
         const velocity = { 
@@ -100,7 +102,7 @@ function spawnEnenies() {
         }
 
         enemies.push(new Enemy(x, y, this.radius, color, velocity))
-        console.log('enemy ' + enemies);
+        //console.log('enemy ' + enemies);
     }, 1000)
         
 }
@@ -108,8 +110,13 @@ function spawnEnenies() {
 //********************************MAIN GAME LOOP ********************************** */
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+
+    c.fillStyle = 'rgba(0, 0, 0, 0.09)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    //c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
+
+
     projectiles.forEach(projectile => {
         projectile.update()
 
@@ -127,9 +134,20 @@ function animate() {
     enemies.forEach(enemy => {
         projectiles.forEach(projectile => {
             if(Math.sqrt(Math.pow(enemy.x - projectile.x, 2) + Math.pow(enemy.y - projectile.y, 2)) < enemy.radius + projectile.radius) {
+                //if enemy's radius is bigger than 10 shrink enemy, if smaller than 7 remove enemy
+                if(enemy.radius - 10 > 10) {
+                    projectiles.splice(projectiles.indexOf(projectile), 1)
+                    score++
+                    gsap.to(enemy, {radius: enemy.radius - 10})
+                    if (enemy.radius < 7) {
+                        enemies.splice(enemies.indexOf(enemy), 1)
+                    }
+                    
+                }else {
                 enemies.splice(enemies.indexOf(enemy), 1)
                 projectiles.splice(projectiles.indexOf(projectile), 1)
                 score++
+                }
             }
         })
     })
@@ -137,7 +155,7 @@ function animate() {
     //Detect if player is hit by enemy, 
     enemies.forEach(enemy => {
         if(Math.sqrt(Math.pow(enemy.x - player.x, 2) + Math.pow(enemy.y - player.y, 2)) < enemy.radius + player.radius) {
-            alert('You lose!')
+            //alert('You lose!')
             //location.reload()
         }
     })
@@ -151,10 +169,10 @@ window.addEventListener('click', (e) => {
 
     const angle = Math.atan2(y - canvasy, x - canvasx)
     const velocity = {
-        x: Math.cos(angle), // * 10,
-        y: Math.sin(angle) //* 10
+        x: Math.cos(angle) * 4.5, // * 10,
+        y: Math.sin(angle) * 4.5//* 10
     }
-    projectiles.push(new Projectiles(canvasx, canvasy, 5, 'red', velocity))
+    projectiles.push(new Projectiles(canvasx, canvasy, 5, 'white', velocity))
 
     console.log("projectile" + projectiles.length);
 })
