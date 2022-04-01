5624e0ac252df24eb84320f85bf210be0edb5082
kdub5184 <kdub5184@gmail.com>
2022-04-01 03:23:53 -0400

    player shoots and removes enemy and projectiles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector('canvas')
+canvas.oncontextmenu = function(e) { e.preventDefault(); e.stopPropagation(); }
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth;
@@ -48,8 +49,8 @@ class Projectiles {
 class Enemy {
     constructor(x, y, radius, color, velocity) {
         this.radius = radius
-        this.x = Math.random() < 0.5 ? -radius : innerWidth + radius
-        this.y = Math.random() < 0.5 ? -radius : innerHeight + radius
+        this.x = x
+        this.y = y
         this.color = color
         this.velocity = velocity
     }
@@ -76,12 +77,31 @@ const player = new Player(canvasx, canvasy, 30, 'blue');
 
 const projectiles = []
 const enemies = []
+let score = 0
 
 function spawnEnenies() {
     setInterval (() => {
-        enemies.push(new Enemy(Math.random() * canvas.width, Math.random() * canvas.height, 30, 'green', {x: Math.random() * 10 - 5, y: Math.random() * 10 - 5}))
-        console.log('enemy');
-    }, 800)
+
+        this.radius = Math.random() * 24 + 6
+        let x, y
+        if(Math.random() > 0.5) {
+            x = Math.random() < 0.5 ? 0-this.radius : canvas.width + this.radius
+            y = Math.random() * canvas.height
+        }else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0-this.radius : canvas.height + this.radius
+        }
+        const color = 'green'
+        const angle = Math.atan2(canvasy - y, canvasx - x)
+
+        const velocity = { 
+            x: Math.cos(angle), 
+            y: Math.sin(angle) 
+        }
+
+        enemies.push(new Enemy(x, y, this.radius, color, velocity))
+        console.log('enemy ' + enemies);
+    }, 1000)
         
 }
 
@@ -92,10 +112,37 @@ function animate() {
     player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
+
+        //if projectile is offscreen remove it
+        if(projectile.x < 0 || projectile.x > canvas.width || projectile.y < 0 || projectile.y > canvas.height) {
+            projectiles.splice(projectiles.indexOf(projectile), 1)
+        }
     })
+
     enemies.forEach(enemy => {
         enemy.update()
     })
+
+    //Check for collsions of all projectiles with all enemies and add point to score
+    enemies.forEach(enemy => {
+        projectiles.forEach(projectile => {
+            if(Math.sqrt(Math.pow(enemy.x - projectile.x, 2) + Math.pow(enemy.y - projectile.y, 2)) < enemy.radius + projectile.radius) {
+                enemies.splice(enemies.indexOf(enemy), 1)
+                projectiles.splice(projectiles.indexOf(projectile), 1)
+                score++
+            }
+        })
+    })
+
+    //Detect if player is hit by enemy, 
+    enemies.forEach(enemy => {
+        if(Math.sqrt(Math.pow(enemy.x - player.x, 2) + Math.pow(enemy.y - player.y, 2)) < enemy.radius + player.radius) {
+            alert('You lose!')
+            //location.reload()
+        }
+    })
+    
+
 }
 
 window.addEventListener('click', (e) => {
@@ -109,7 +156,7 @@ window.addEventListener('click', (e) => {
     }
     projectiles.push(new Projectiles(canvasx, canvasy, 5, 'red', velocity))
 
-    console.log("projectile");
+    console.log("projectile" + projectiles.length);
 })
 
 animate()
